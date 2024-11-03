96e468dc389f8b7ccce24add6abc88873267bd9b
john-mc7800 <130221320+john-mc7800@users.noreply.github.com>
2024-11-03 14:42:24 +0500

    create userModel schema

---
+++ src/models/userModels.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+ const userSchema = new mongoose.Schema({
+  username:{
+    type:String,
+    required:[true,'Enter a username'],
+    unique:[true,'username unavailable'],
+  },
+  email:{
+    type:String,
+    required:[true,'Enter a email'],
+    unique:[true,'email already in use'],
+  },
+  password:{
+    type:String,
+    required:[true,'Enter a password'],
+  },
+  isVarified:{
+    type:Boolean,
+    default:false,
+  },
+  isAdmin:{
+    type:Boolean,
+    default:false,
+  },
+  forgotPasswordToken:string,
+  forgotPasswordExpiresToken:Date,
+  verifyToken:string,
+  varifyTokenExpiry:Date,
+ })
+  const User= mongoose.models.user || mongoose.model('user',userSchema)
+  export default User;
